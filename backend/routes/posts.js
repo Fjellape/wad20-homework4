0b0b2c6fc3d961de45af708b39374bb9f1d76381
Kristo V <smileshow2013@gmail.com>
2020-12-12 15:06:47 +0200

    Task2 and Task3 #done

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -25,19 +25,25 @@ router.get('/', authorize, (request, response) => {
 router.post('/', authorize,  (request, response) => {
 
     // Endpoint to create a new post
-
 });
 
 
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+    PostModel.like(request.currentUser.id,request.params.postId, () => {
+        response.status(201)
+    });
+    response.json([])
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
-
+    PostModel.unlike(request.currentUser.id, request.params.postId,() => {
+        response.status(201)
+    });
+    response.json([])
 });
 
 module.exports = router;
